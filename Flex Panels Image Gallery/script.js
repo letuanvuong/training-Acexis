80ecf7472bb7a2c746c102fce2573c3d67aabc26
TUANVUONG\A <vuonggl.it@gmail.com>
2019-11-18 17:15:07 +0700

    update flex

--- Flex Panels Image Gallery/script.js
+++ Flex Panels Image Gallery/script.js
@@ -7,7 +7,6 @@ function addOpen(){
 panels.forEach(panel => panel.addEventListener('click', addOpen))
 
 function addActive(e) {
-  console.log(e.propertyName);
   if(e.propertyName.includes('flex')){
     this.classList.toggle('open-active')
   }
